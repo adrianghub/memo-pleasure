6f41c19e2454fa12622f0520e346b16eb11a9212
Adrian <adrian_zinko@op.pl>
2020-10-29 13:20:35 +0100

    feat: validate search input

--- routes/locations.js
+++ routes/locations.js
@@ -1,39 +1,45 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const Location = require('../models/location')
-
+const Location = require("../models/location");
 
 // all locations
-router.get('/', async (req, res) => {
-  try {
-    const locations = await Location.find({})
-    res.render('locations/index', { locations: locations })
-  } catch {
-    res.redirect('/')
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.country !== null && req.query.country !== "") {
+    searchOptions.country = new RegExp(req.query.country, "i");
   }
-})
+  try {
+    const locations = await Location.find(searchOptions);
+    res.render("locations/index", {
+      locations: locations,
+      searchOptions: req.query,
+    });
+  } catch {
+    res.redirect("/");
+  }
+});
 
 // new location
-router.get('/new', (req, res) => {
-  res.render('locations/new', { location: new Location() })
-})
+router.get("/new", (req, res) => {
+  res.render("locations/new", { location: new Location() });
+});
 
 // add new location
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const location = new Location({
-    country: req.body.country
-  })
+    country: req.body.country,
+  });
   try {
-    const newLocation = await location.save()
+    const newLocation = await location.save();
     // res.redirect('authors/${newLocation.id}')
-    res.redirect('locations')
+    res.redirect("locations");
   } catch (err) {
-    res.render('locations/new', {
+    res.render("locations/new", {
       location: location,
-      errMessage: `Error happend: ${err}`
-    })
+      errMessage: `Error happend: ${err}`,
+    });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
