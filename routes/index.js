7efca95670a52bc8337cfd84d6831786e47e2d7f
Adrian <adrian_zinko@op.pl>
2020-10-31 10:15:30 +0100

    feat: display pictures on main index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Picture = require('../models/picture')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  let pictures 
+  try {
+    pictures = await Picture.find().sort({ createdAt: 'desc' }).limit(10).exec()
+  } catch {
+    pictures = []
+  }
+  res.render('index', { pictures: pictures })
 })
 
 module.exports = router
